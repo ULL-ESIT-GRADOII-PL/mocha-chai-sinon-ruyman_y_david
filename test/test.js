83beab73f9f1270145fe61cbe1fb94657cd995b9
Ruymán <alu0100845235@ull.edu.es>
2016-03-17 13:42:21 +0000

    Añadidos nuevos test

--- test/test.js
+++ test/test.js
@@ -1,5 +1,41 @@
 var expect = chai.expect;
 
+describe("Medida", function() {
+  describe("Constructor", function() {
+    it("Debería tener un tipo y un valor", function() {
+      var m = new Medida (5, 'M');
+      expect(m.valor).to.equal(5);
+      expect(m.tipo).to.equal("M");
+    });
+    it("Debería crearse si sólo le pasamos 1 parámetro", function() {
+      var m = new Medida (5);
+      expect(m.valor).to.equal(5);
+      expect(m).to.not.be.undefined;
+    });
+    it("No debería crearse si no le pasamos parámetros", function() {
+      var m = new Medida ();
+      expect(m).to.be.undefined;
+    });
+  });
+  describe("#convertir", function() {
+    it("Debería devolver un String", function() {
+      expect(Medida.convertir(null)).to.be.a('String');
+    });
+    it("Debería devolver un string 'Error...' expresión no válida", function() {
+        expect(Medida.convertir (null)).to.equal("Introduzca una temperatura valida: 330e-1 Fahrenheit to Celsius");
+    });
+    it("Debería devolver un string 'Error...' no se encuentra destino", function() {
+        expect(Medida.convertir ("100 C to JJJJJ")).to.equal('Desconozco como convertir desde "Celsius" hasta "JJJJJ"';);
+    });
+    it("Debería devolver un string con la conversión realizada", function() {
+        expect(Medida.convertir ("100 C to K")).to.equal("373.15 Kelvin");
+    });
+    it("Debería devolver un string con la conversión realizada", function() {
+        expect(Medida.convertir ("100 F K")).to.equal("310.93 Kelvin");
+    });
+  });
+});
+
 describe("Longitud", function() {
   describe("Constructor", function() {
     it("Debería tener un tipo y un valor", function() {
@@ -12,17 +48,28 @@ describe("Longitud", function() {
     describe("Constructor", function() {
       it("Debería tener un tipo M y un valor", function() {
         var m = new Metros(5);
-        expect(m.valor).to.equal(5);
+        expect(m.toString).to.equal(5);
         expect(m.tipo).to.equal("M");
       });
     });
 
     describe("#toPulgadas", function() {
-        it("Debería convertirse en Pulgadas", function() {
-          var pulg = (new Metros(5)).toPulgadas();
-          expect(Math.round(pulg.valor * 100) / 100).to.equal(196.85);
-        });
+      it("Debería convertirse en Pulgadas", function() {
+        var pulg = (new Metros(5)).toPulgadas();
+        expect(Math.round(pulg.valor * 100) / 100).to.equal(196.85);
       });
+    });
+
+    describe("#toString", function() {
+      it("Debería devolver un String", function() {
+        var m = new Metros (5);
+        expect(m.toString ()).to.be.a('String');
+      });
+      it("Debería devolver un string '5 Metros' ", function() {
+        var m = new Metros (5);
+        expect(m.toString ()).to.equal("5 Metros");
+      });
+    });
   });
 
   describe("Pulgadas", function() {
@@ -39,7 +86,18 @@ describe("Longitud", function() {
           var pulg = (new Pulgadas(196.85)).toMetros();
           expect(Math.round(pulg.valor)).to.equal(5);
         });
+    });
+
+    describe("#toString", function() {
+      it("Debería devolver un String", function() {
+        var p = new Pulgadas (5);
+        expect(p.toString ()).to.be.a('String');
       });
+      it("Debería devolver un string '5 Pulgadas' ", function() {
+        var p = new Pulgadas (5);
+        expect(p.toString ()).to.equal("5 Pulgadas");
+      });
+    });
   });
 });
 
@@ -66,14 +124,25 @@ describe("Temperatura", function() {
           var far = (new Celsius(1)).toFahrenheit();
           expect(Math.round(far.valor * 100) / 100).to.equal(33.8);
         });
-      });
+    });
 
-      describe("#toKelvin", function() {
-        it("Debería convertirse en Kelvin", function() {
-          var kel = (new Celsius(1)).toKelvin();
-          expect(Math.round(kel.valor * 100) / 100).to.equal(274.15);
-        });
+    describe("#toKelvin", function() {
+      it("Debería convertirse en Kelvin", function() {
+        var kel = (new Celsius(1)).toKelvin();
+        expect(Math.round(kel.valor * 100) / 100).to.equal(274.15);
       });
+    });
+
+    describe("#toString", function() {
+      it("Debería devolver un String", function() {
+        var c = new Celsius (5);
+        expect(c.toString ()).to.be.a('String');
+      });
+      it("Debería devolver un string '5 Celsius' ", function() {
+        var c = new Celsius (5);
+        expect(c.toString ()).to.equal("5 Celsius");
+      });
+    });
   });
 
   describe("Fahrenheit", function() {
@@ -92,12 +161,23 @@ describe("Temperatura", function() {
         });
       });
 
-      describe("#toKelvin", function() {
-        it("Debería convertirse en Kelvin", function() {
-          var kel = (new Fahrenheit(1)).toKelvin();
-          expect(Math.round(kel.valor * 100) / 100).to.equal(255.93);
-        });
+    describe("#toKelvin", function() {
+      it("Debería convertirse en Kelvin", function() {
+        var kel = (new Fahrenheit(1)).toKelvin();
+        expect(Math.round(kel.valor * 100) / 100).to.equal(255.93);
       });
+    });
+
+    describe("#toString", function() {
+      it("Debería devolver un String", function() {
+        var f = new Fahrenheit (5);
+        expect(f.toString ()).to.be.a('String');
+      });
+      it("Debería devolver un string '5 Fahrenheit' ", function() {
+        var f = new Fahrenheit (5);
+        expect(f.toString ()).to.equal("5 Fahrenheit");
+      });
+    });
   });
 
   describe("Kelvin", function() {
@@ -116,11 +196,22 @@ describe("Temperatura", function() {
         });
       });
 
-      describe("#toCelsius", function() {
-        it("Debería convertirse en Celsius", function() {
-          var cel = (new Kelvin(1)).toCelsius();
-          expect(Math.round(cel.valor * 100) / 100).to.equal(-272.15);
-        });
+    describe("#toCelsius", function() {
+      it("Debería convertirse en Celsius", function() {
+        var cel = (new Kelvin(1)).toCelsius();
+        expect(Math.round(cel.valor * 100) / 100).to.equal(-272.15);
       });
+    });
+
+    describe("#toString", function() {
+      it("Debería devolver un String", function() {
+        var k = new Kelvin (5);
+        expect(k.toString ()).to.be.a('String');
+      });
+      it("Debería devolver un string '5 Kelvin' ", function() {
+        var k = new Kelvin (5);
+        expect(k.toString ()).to.equal("5 Kelvin");
+      });
+    });
   });
 });
